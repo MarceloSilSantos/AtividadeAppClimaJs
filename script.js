d044240336c1a87bf4cffdb62cec8e89814b3c37
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-04-24 12:10:21 -0300

    code update

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-var a = 35;
-var b = 90;
-resultado = (2*a)+b;
-console.log(resultado);
-alert (resultado);
-// var a = 20;
+// command line - npm install react-scripts@latest
+// npm install axios
+//https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid={chave}&units=metric
+//via browser - https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid=999a9f3a09cc0480b478add5d0b9be86&units=metric
 
-// var b = 25;
+const chave = '999a9f3a09cc0480b478add5d0b9be86'
+const url = `https://api.openweathermap.org/data/2.5/weather?q=São Paulo&lang=pt_br&appid=${chave}&units=metric`
+
+novaTemp() {
+    return console.log(temperatura);
+}
+
+const obterTemp = () =>{
+
+  fetch(url)
+        .then(resposta =>{
+          console.log(resposta)
+          return resposta.json()
+        })
+          .then(dados =>{
+            console.log(dados)
+            const temp = dados.main.temp;
+
+          })
+          .catch(erro => {
+            console.log(erro)
+          })
+ }
+
+ 
+ 
